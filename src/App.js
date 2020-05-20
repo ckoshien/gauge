dc288b43bdfc266b684278a102459a6dd262c4be
ckoshien <ckoshien@gmail.com>
2020-05-21 00:00:58 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,174 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [count , setCount ] = useState(3000);
+  useEffect(()=>{
+    setTimeout(()=>{
+      if(count < 15000){
+        setCount(count + 10);
+      }
+    },20)
+  },[count]);
+
+  const _1stColorBox = () => {
+      return(
+        <div
+        style={{
+          position:'absolute',
+          top:0,
+          left:0,
+          background:'linear-gradient(to right top, #65eefe ,#1fa4fb)',
+          height:250,
+          width:360
+        }}
+      ></div>
+      );
+  }
+
+
+  const _2ndColorBox = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:250,
+          left:0,
+          background:'linear-gradient(to left top, white 20%, #65eefe)',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
+  const shadowBox = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:300,
+          left:0,
+          backgroundColor:'white',
+          height:500,
+          width:420,
+          transform: `rotate(${count/100+40}deg)`,
+          transformOrigin:'right 10px'
+        }}
+      ></div>
+    );
+  }
+  const shadowBox2 = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:300,
+          left:0,
+          backgroundColor:'white',
+          height:500,
+          width:400,
+          transform: `rotate(${count/100+130}deg)`,
+          transformOrigin:'right 10px'
+        }}
+      ></div>
+    );
+  }
+
+  const _3rdColorBox=()=>{
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:0,
+          left:360,
+          background:'linear-gradient(to right, #1fa4fb 40% , blue)',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
+  const _4thColorBox =()=>{
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:250,
+          left:360,
+          backgroundColor:'white',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        position:'relative',
+        filter:`grayscale(${100-(count/100)}%)`
+      }}
+    >
+      {_1stColorBox()} 
+      {_2ndColorBox()}
+      
+      {_3rdColorBox()}
+      {_4thColorBox()}
+      {shadowBox()}
+      {shadowBox2()}
+      
+      <img
+      style={{
+        position:'absolute',
+        top:0,
+        left:0,
+      }}
+      src={'/images/gauge.png'} />
+      <div
+        style={{
+          fontSize:70,
+          position:'absolute',
+          top:220,
+          left:180
+        }}
+      >
+        <span
+          style={{
+            fontSize:70
+          }}
+        >{(count/100).toString().split('.')[0]}</span>
+        <span
+          style={{
+            fontSize:40
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          .{(count/100).toFixed(2).toString().split('.')[1]}/100
+        </span>
+      </div>
+      <div
+        style={{
+          fontSize:70,
+          position:'absolute',
+          top:210,
+          left:170,
+          opacity:0.15
+        }}
+      >
+        <span
+          style={{
+            fontSize:70
+          }}
+        >{(count/100).toString().split('.')[0]}</span>
+        <span
+          style={{
+            fontSize:40
+          }}
+        >
+          .{(count/100).toFixed(2).toString().split('.')[1]}/100
+        </span>
+      </div>
     </div>
   );
 }
