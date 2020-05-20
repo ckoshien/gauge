b1a8d14a68a08450bdec1d9230cb99f46b9cfa88
ckoshien <ckoshien@gmail.com>
2020-05-21 00:06:39 +0900

    Updates

---
+++ precache-manifest.1dcf1941e6b51701cb8638c196af66b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b286278fc509560685f75279149e3b",
+    "url": "/gauge/index.html"
+  },
+  {
+    "revision": "1098c89cff6a5de6f1a2",
+    "url": "/gauge/static/css/main.4ef80227.chunk.css"
+  },
+  {
+    "revision": "547620171ca15bd68ce7",
+    "url": "/gauge/static/js/2.d562e172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gauge/static/js/2.d562e172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1098c89cff6a5de6f1a2",
+    "url": "/gauge/static/js/main.2dd1d013.chunk.js"
+  },
+  {
+    "revision": "f384abc0437a89a0ec82",
+    "url": "/gauge/static/js/runtime-main.78eb4e82.js"
+  }
+]);
