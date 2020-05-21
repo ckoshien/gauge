bdc8cdf8d67573365461b0a9578948db07d3e28f
ckoshien <ckoshien@gmail.com>
2020-05-22 00:50:53 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,193 +1,104 @@
 import React, { useEffect, useState, useCallback } from 'react';
 
 function App() {
-  const [count , setCount ] = useState(3000);
+  const [ r, setR ] = useState(0.5);
+  const [ l, setL ] = useState(0.5);
   useEffect(()=>{
     setTimeout(()=>{
-      if(count < 15000){
-        setCount(count + 10);
+      //setY(0.5)
+      if(r < 0.8){
+        setR(r + 0.01);
+      }else if(Math.floor((l*100).toFixed(0))/100 < 1.0){
+        setR(0.8 + 0.2*Math.random());
+      }else{
+        setR(1);
       }
-    },20)
-  },[count]);
-
-  const _1stColorBox = () => {
-      return(
-        <div
-        style={{
-          position:'absolute',
-          top:0,
-          left:0,
-          background:'linear-gradient(to right top, #65eefe ,#1fa4fb)',
-          height:250,
-          width:360
-        }}
-      ></div>
-      );
-  }
-
-
-  const _2ndColorBox = () => {
-    return(
+      if(l<0.8){
+        setL(r + 0.01);
+      }else if(Math.floor((l*100).toFixed(0))/100 < 1.0){
+        setL(0.8 + 0.2*Math.random());
+      }else{
+        setL(1);
+        //console.log(l);
+      }
+    },250)
+  },[r])
+  
+  return(
+    <div className="status">
       <div
         style={{
           position:'absolute',
-          top:250,
-          left:0,
-          background:'linear-gradient(to left top, white 20%, #65eefe)',
-          height:240,
-          width:360
-        }}
-      ></div>
-    );
-  }
-
-  const shadowBox = () => {
-    return(
-      <div
-        style={{
-          position:'absolute',
-          top:300,
-          left:0,
-          backgroundColor:'white',
-          height:500,
-          width:420,
-          transform: `rotate(${count/100+40}deg)`,
-          transformOrigin:'right 10px'
-        }}
-      ></div>
-    );
-  }
-  const shadowBox2 = () => {
-    return(
-      <div
-        style={{
-          position:'absolute',
-          top:300,
-          left:0,
           backgroundColor:'white',
           height:500,
           width:400,
-          transform: `rotate(${count/100+130}deg)`,
-          transformOrigin:'right 10px'
+          top:-100-300*r,
+          left:0
         }}
       ></div>
-    );
-  }
-
-  const _3rdColorBox=()=>{
-    return(
       <div
         style={{
           position:'absolute',
-          top:0,
-          left:360,
-          background:'linear-gradient(to right, #1fa4fb 40% , blue)',
-          height:240,
-          width:360
-        }}
-      ></div>
-    );
-  }
-
-  const _4thColorBox =()=>{
-    return(
-      <div
-        style={{
-          position:'absolute',
-          top:250,
-          left:360,
           backgroundColor:'white',
-          height:240,
-          width:360
+          height:500,
+          width:400,
+          top:-100-300*l,
+          left:400
         }}
       ></div>
-    );
-  }
-
-  let fontColor = useCallback(()=>{
-    if(count<10000){
-      return 'black';
-    }else if(count > 10000 && count < 15000){
-      return 'blue';
-    }else{
-      return 'red'
-    }
-  },[count])
-
-  return (
-    <div
-      style={{
-        position:'relative',
-        filter:`grayscale(${100-(count/100)}%)`
-      }}
-    >
-      {_1stColorBox()} 
-      {_2ndColorBox()}
-      
-      {_3rdColorBox()}
-      {_4thColorBox()}
-      {shadowBox()}
-      {shadowBox2()}
-      
       <img
+        src={process.env.PUBLIC_URL + '/images/status_gauge.png'} 
+        style={{
+          height:500,
+          position:'absolute'
+        }}  
+      />
+     <span
       style={{
         position:'absolute',
-        top:0,
-        left:0,
+        top:150,
+        left:200
       }}
-      src={'/images/gauge.png'} />
-      <div
+     >
+       {(r*100).toFixed(0)}%
+      </span>
+     <span
+      style={{
+        position:'absolute',
+        top:150,
+        left:640
+      }}
+     >
+       {(l*100).toFixed(0)}%
+     </span>
+     {
+       (r === 1 && l === 1)
+       ?
+       <div
         style={{
-          fontSize:70,
           position:'absolute',
-          top:220,
-          left:180
+          background:'linear-gradient(to top, blue, #3efefc)',
+          height:80,
+          width:400,
+          top: 230,
+          left:250,
+          opacity:0.7,
+          color:'white',
+          fontSize:18,
+          textAlign:'center'
         }}
       >
+        O-RAISER
+        <br/>
         <span
           style={{
-            fontSize:70,
-            color:fontColor()
+            fontSize:36
           }}
-        >{(count/100).toString().split('.')[0]}</span>
-        <span
-          style={{
-            fontSize:40,
-            color:fontColor()
-          }}
-        >
-          .{(count/100).toFixed(2).toString().split('.')[1]}
-         </span> 
-         <span
-          style={{
-            fontSize:40
-          }}
-         >
-          /100
-        </span>
+        >RAISER SYS</span>
       </div>
-      <div
-        style={{
-          fontSize:70,
-          position:'absolute',
-          top:210,
-          left:170,
-          opacity:0.15
-        }}
-      >
-        <span
-          style={{
-            fontSize:70
-          }}
-        >{(count/100).toString().split('.')[0]}</span>
-        <span
-          style={{
-            fontSize:40
-          }}
-        >
-          .{(count/100).toFixed(2).toString().split('.')[1]}/100
-        </span>
-      </div>
+      :''
+     }
+     
     </div>
   );
 }
---
+++ src/Charge.js
@@ -0,0 +1,195 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
+function Charge() {
+  const [count , setCount ] = useState(3000);
+  useEffect(()=>{
+    setTimeout(()=>{
+      if(count < 15000){
+        setCount(count + 10);
+      }
+    },20)
+  },[count]);
+
+  const _1stColorBox = () => {
+      return(
+        <div
+        style={{
+          position:'absolute',
+          top:0,
+          left:0,
+          background:'linear-gradient(to right top, #65eefe ,#1fa4fb)',
+          height:250,
+          width:360
+        }}
+      ></div>
+      );
+  }
+
+
+  const _2ndColorBox = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:250,
+          left:0,
+          background:'linear-gradient(to left top, white 20%, #65eefe)',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
+  const shadowBox = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:300,
+          left:0,
+          backgroundColor:'white',
+          height:500,
+          width:420,
+          transform: `rotate(${count/100+40}deg)`,
+          transformOrigin:'right 10px'
+        }}
+      ></div>
+    );
+  }
+  const shadowBox2 = () => {
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:300,
+          left:0,
+          backgroundColor:'white',
+          height:500,
+          width:400,
+          transform: `rotate(${count/100+130}deg)`,
+          transformOrigin:'right 10px'
+        }}
+      ></div>
+    );
+  }
+
+  const _3rdColorBox=()=>{
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:0,
+          left:360,
+          background:'linear-gradient(to right, #1fa4fb 40% , blue)',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
+  const _4thColorBox =()=>{
+    return(
+      <div
+        style={{
+          position:'absolute',
+          top:250,
+          left:360,
+          backgroundColor:'white',
+          height:240,
+          width:360
+        }}
+      ></div>
+    );
+  }
+
+  let fontColor = useCallback(()=>{
+    if(count<10000){
+      return 'black';
+    }else if(count > 10000 && count < 15000){
+      return 'blue';
+    }else{
+      return 'red'
+    }
+  },[count])
+
+  return (
+    <div
+      style={{
+        position:'relative',
+        filter:`grayscale(${100-(count/100)}%)`
+      }}
+    >
+      {_1stColorBox()} 
+      {_2ndColorBox()}
+      
+      {_3rdColorBox()}
+      {_4thColorBox()}
+      {shadowBox()}
+      {shadowBox2()}
+      
+      <img
+      style={{
+        position:'absolute',
+        top:0,
+        left:0,
+      }}
+      src={process.env.PUBLIC_URL + '/images/charge_gauge.png'} />
+      <div
+        style={{
+          fontSize:70,
+          position:'absolute',
+          top:220,
+          left:180
+        }}
+      >
+        <span
+          style={{
+            fontSize:70,
+            color:fontColor()
+          }}
+        >{(count/100).toString().split('.')[0]}</span>
+        <span
+          style={{
+            fontSize:40,
+            color:fontColor()
+          }}
+        >
+          .{(count/100).toFixed(2).toString().split('.')[1]}
+         </span> 
+         <span
+          style={{
+            fontSize:40
+          }}
+         >
+          /100
+        </span>
+      </div>
+      <div
+        style={{
+          fontSize:70,
+          position:'absolute',
+          top:210,
+          left:170,
+          opacity:0.15
+        }}
+      >
+        <span
+          style={{
+            fontSize:70
+          }}
+        >{(count/100).toString().split('.')[0]}</span>
+        <span
+          style={{
+            fontSize:40
+          }}
+        >
+          .{(count/100).toFixed(2).toString().split('.')[1]}/100
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Charge;
